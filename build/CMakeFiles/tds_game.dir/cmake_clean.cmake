c329a53f2c848f3b0855829265018e8162896938
tdh0ang <td.hoang03@gmail.com>
2024-11-27 15:46:46 +0100

    Added build files

---
+++ build/CMakeFiles/tds_game.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tds_game.dir/link.d"
+  "CMakeFiles/tds_game.dir/src/main.cpp.o"
+  "CMakeFiles/tds_game.dir/src/main.cpp.o.d"
+  "tds_game"
+  "tds_game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tds_game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/tds_game.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for tds_game.
+# This may be replaced when dependencies are built.
